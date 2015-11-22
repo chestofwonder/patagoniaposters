c1839289f69aa99887b95c017afbd3af152c9126
chestofwonder <miguelarroyo@MacBook-Pro-de-Miguel.local>
2015-11-22 19:44:30 +0100

    Ignore db_connection file

--- web/db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysql_connect("localhost", "tagoni81_usuario", "Usuario1234");
-
-if (! $con){die ("ERROR EN LA CONEXION CON MYSQL: ".mysql_error());}
-
-$base = mysql_select_db("tagoni81_datos", $con);
-if(! $base){die ("ERROR AL CONECTAR CON LA BASE DE DATOS: ".mysql_error());}
-?>
