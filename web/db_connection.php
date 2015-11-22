1dbb49f132d821138a05ebe474497ccc148871e8
chestofwonder <miguelarroyo@MacBook-Pro-de-Miguel.local>
2015-11-22 11:38:00 +0100

    Add external db connection

---
+++ web/db_connection.php
@@ -0,0 +1,7 @@
+<?php
+
+// $con = mysql_connect("localhost", "tagoni81_usuario", "Usuario1234");
+$con = mysql_connect("localhost", "root", "root");
+$base = mysql_select_db("tagoni81_datos", $con);
+
+?>
