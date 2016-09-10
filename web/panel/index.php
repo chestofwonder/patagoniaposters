25d2893aff808b01d44d2faa516288175694dcb0
chestofwonder <miguelarroyo@MacBook-Pro-de-Miguel-2.local>
2016-09-10 11:55:41 +0200

    Hotjar code

--- web/panel/index.php
+++ web/panel/index.php
@@ -22,6 +22,17 @@ session_unset;
 				<link rel="stylesheet" href="/css/security.css" type="text/css" media="all">
 				<script src='../js/jquery.js' type='text/javascript'></script>
 				<script src='../js/custom/panel.js' type='text/javascript'></script>
+<!-- Hotjar Tracking Code for http://patagoniaposters.cl/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:281932,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 		</head>
 		<body>
 				<header id="panel_options">
